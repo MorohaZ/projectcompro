9c182c03b14b51c7435fe5c0c0defdf0e59b17ba
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2024-02-15 10:35:27 -0800

    Add files via upload

---
+++ projeccompro/comp1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/Network.hpp>
+
+int main()
+{	//window
+	sf::Window window(sf::VideoMode(800,600),"love game",sf::Style::Titlebar|sf::Style::Close);
+	sf::Event ev;
+	
+	//gameloop
+
+	return 0;
+
+}
