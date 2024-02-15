d20237c941073aec5ccd621e2b8f6a763f11e236
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2024-02-15 10:47:57 -0800

    Add files via upload

--- projeccompro/comp1.cpp
+++ projeccompro/comp1.cpp
@@ -8,10 +8,40 @@
 
 int main()
 {	//window
-	sf::Window window(sf::VideoMode(800,600),"love game",sf::Style::Titlebar|sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(800,600),"love game",sf::Style::Titlebar|sf::Style::Close);
 	sf::Event ev;
 	
 	//gameloop
+	while (window.isOpen())
+	{
+		//event polling
+		while (window.pollEvent(ev))
+		{
+			switch (ev.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (ev.key.code == sf::Keyboard::Escape)
+				{
+					window.close();
+				}
+				break;
+			}
+		}
+
+		//update
+		
+
+		//render
+		window.clear();//clear old frame
+		window.display();//tell app that window is done drawing
+
+	}
+
+	//end of appication
+
 
 	return 0;
 
